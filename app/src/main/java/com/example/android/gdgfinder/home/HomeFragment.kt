a2217f62bf9fefbc63a386fd828f90bde7973b88
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-04-21 05:31:04 -0700

    Step.04-Solution-Add-Floating-Action-Button:#Udacity

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,15 +23,23 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // TODO (06) Create a binding to the home_fragment layout and tell the binding
-        // about the viewModel.
 
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+         val binding=HomeFragmentBinding.inflate(inflater)
+
+
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-
-        // TODO (07)  Register an observer on navigateToSearch, and have it navigate
+        binding.viewModel=viewModel
+        //   Register an observer on navigateToSearch, and have it navigate
         // to gdgListFragment if shouldNavigate is true.
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer {navigate->
+            if (navigate){
+                val navController=findNavController()
+                navController.navigate(R.id.action_homeFragment_to_addGdgFragment)
+                viewModel.onNavigatedToSearch()
+            }
 
-        return view
+        })
+
+        return binding.root
     }
 }
